4ce4cdca80745060305a6e66b6fe1046c763fc5f
Roshnie <rainyray404@gmail.com>
2026-08-19 11:15:51 +0530

    Day12_changes

---
+++ src/components/AboutSection/AboutSection.jsx
@@ -0,0 +1,19 @@
+import './AboutSection.css';
+
+function AboutSection() {
+  return (
+    <section id='about' className='about-section'>
+      <div className='about-wrapper container'>
+        <div className='about-image'>
+          <img src='meow.jpg' alt='Roshnie Rai' />
+        </div>
+        <div className='about-copy'>
+          <h1>About Me</h1>
+          <p>Hi! I'm Roshnie, a developer based in India building fast, accessible web experiences.</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AboutSection;
