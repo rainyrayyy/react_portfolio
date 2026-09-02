287d68a10283ee2ed88702712fb6c4151e5465c0
Roshnie <rainyray404@gmail.com>
2026-09-02 22:10:34 +0530

    edit...

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -5,7 +5,7 @@ function AboutSection() {
     <section id='about' className='about-section'>
       <div className='about-wrapper container'>
         <div className='about-image'>
-          <img src='/latest headshot.jpg' alt='Roshnie Rai' />
+          <img src='/pfpimg.jpeg' alt='Roshnie Rai' />
         </div>
         <div className='about-copy'>
           <h1>About Me</h1>
